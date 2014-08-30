53310b93136a8f6c74512f7efb7b4d0d8de36643
ffasfasf <gfsdfa@aaa.com>
2014-08-30 09:44:54 +0300

    json

--- app/templates/bap/utils/JsonEasyFilter.js
+++ app/templates/bap/utils/JsonEasyFilter.js
@@ -1,6 +1,21 @@
 var traverse = require("traverse");
 
+var _getPathStr = function(path, delimiter){
+	if(!delimiter){
+		delimiter = '.';
+	}
+	var res = '';
+	for(var i = 0; i<path.length; i++){
+		if(i!==0){
+			res+=delimiter;
+		}
+		res+=path[i];
+	}
+	return res;
+};
+
 var JsonNode = function(){
+	this._nodeHash = null;
 	this.key = null;
 	this.value = null;
 	this.parent = null;
@@ -10,39 +25,67 @@ var JsonNode = function(){
 	this.circular = null;
 	this.level = null;
 	this.getPathStr = function(delimiter){
-		if(!delimiter){
-			delimiter = '.';
-		}
-		var res = '';
-		for(var i = 0; i<this.path.length; i++){
-			if(i!==0){
-				res+=delimiter;
-			}
-			res+=this.path[i];
-		}
-		return res;
+		return _getPathStr(this.path, delimiter);
 	};
+	this.has = function(key){
+		if(!this.value){
+			return false;
+		}
+		if(this.value[key]!==undefined){
+			return true;
+		} else{
+			return false;
+		}
+	};
+	
+	this.hasOwnProperty = function(key){
+		if(!this.value){
+			return false;
+		}
+		return this.value.hasOwnProperty(key);
+	}
 };
 
 
-module.exports = {
-	
-	filter: function(obj, callback){
+module.exports = new function(){
+	this.filter = function(obj, callback){
 		var result = [];
+		var nodeHash = {};
 		traverse(obj).forEach(function(val){
 			// console.log(val);
 			var node = new JsonNode();
+			node._nodeHash = nodeHash;
 			node.path = this.path;
 			node.key = this.key;
 			node.value = this.node;
 			node.level = this.level;
 			node.isRoot = this.isRoot;
 			var resCallBack = callback(node);
+			
+			// Hash
+			nodeHash[node.getPathStr()] = node;
+			
+			// Parent
+			var parentPath = node.path.slice(0, node.path.length - 1);
+			var parentNode = nodeHash[_getPathStr(parentPath)];
+			if(parentNode){
+				node.parent = parentNode;
+			}
+			
 			if(resCallBack){
 				result.push(resCallBack);
 			}
 		});
 		return result;
-	}
+	};
+	this.root = function(obj){
+		var rootArray = this.filter(obj, function(node){
+			if(node.isRoot){
+				return node;
+			}
+		});
+		
+		return rootArray[0];
+	};
+};
 
-}
--- app/templates/bap/utils/JsonEasyFilterTests.js
+++ app/templates/bap/utils/JsonEasyFilterTests.js
@@ -2,67 +2,87 @@ var JsonEasyFilter = require('./JsonEasyFilter');
 
 var json = {
 	departments : {
-		admin: {
-			name: "Administrative",
-			manager: 'john'
+		admin : {
+			name : "Administrative",
+			manager : 'john'
 		},
-		it: {
-			name: 'IT',
-			manager: 'andy'
+		it : {
+			name : 'IT',
+			manager : 'andy'
 		},
-		finance: {
-			name: 'Financiar',
-			manager: 'anna'
+		finance : {
+			name : 'Financiar',
+			manager : 'anna'
 		}
 	},
-	employees: [
-		{
-			username: 'john',
-			firstName: 'John',
-			lastName: 'JOHN',
-			salary: 150,
-			gender: 'M',
-			birthDate: '1980/05/21'
-		},
-		{
-			username: 'andy',
-			firstName: 'Andy',
-			lastName: 'ANDY',
-			salary: 200,
-			gender: 'M',
-			birthDate: '1985/10/30'
-		},
-		{
-			username: 'anna',
-			firstName: 'Anna',
-			lastName: 'ANNA',
-			salary: 300,
-			gender: 'F',
-			birthDate: '1989/08/05'
-		},
-		{
-			username: 'gaby',
-			firstName: 'Gaby',
-			lastName: 'GABY',
-			salary: 400,
-			gender: 'M',
-			birthDate: '1993/11/20'
-		},
-	]
+	employees : [ {
+		username : 'john',
+		firstName : 'John',
+		lastName : 'JOHN',
+		salary : 150,
+		gender : 'M',
+		birthDate : '1980/05/21'
+	}, {
+		username : 'andy',
+		firstName : 'Andy',
+		lastName : 'ANDY',
+		salary : 200,
+		gender : 'M',
+		birthDate : '1985/10/30'
+	}, {
+		username : 'anna',
+		firstName : 'Anna',
+		lastName : 'ANNA',
+		salary : 300,
+		gender : 'F',
+		birthDate : '1989/08/05'
+	}, {
+		username : 'gaby',
+		firstName : 'Gaby',
+		lastName : 'GABY',
+		salary : 400,
+		gender : 'M',
+		birthDate : '1993/11/20'
+	}, {
+		username : null,
+		firstName : 'Gaby',
+		lastName : 'GABY',
+		salary : 400,
+		gender : 'M',
+		birthDate : '1993/11/20'
+	} ]
 };
 
 /**
- * Get all employees with a salary over 200.
+ * Get all employee username with a salary over 200.
  */
-function test1(){
-	JsonEasyFilter.filter(json, function(node){
-		if(node.key=='employees'){
-			JsonEasyFilter.filter(node.value);
+function test1() {
+	var res = JsonEasyFilter.filter(json, function(node) {
+		if (node.hasOwnProperty('username')) {
+			return node.value.username;
 		}
 	});
+	console.log(res);
 };
 
-	JsonEasyFilter.filter(json.employees, function(node){
-		console.log(node.key);
-	});
+//test1();
+
+var x = JsonEasyFilter.root(json);
+console.log(x.value);
+
+
+// JsonEasyFilter.filter(json, function(node) {
+// console.log(node.path + ' ' + node.key);
+// });
+
+//console.log(json.employees[4].hasOwnProperty('username'));
+//var res = JsonEasyFilter.filter(json, function(node) {
+//	if (node.hasOwnProperty('username')) {
+//		return node.value.username;
+//	}
+//});
+//console.log(res);
+//var a = ['a', 'b', 'c'];
+//console.log(a.slice(0, a.length-1));
+
 
